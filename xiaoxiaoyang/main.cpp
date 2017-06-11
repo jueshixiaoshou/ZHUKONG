74d663284a41015f7f682a983cd724a989c57265
qq1433255094 <qq1433255094@outlook.com>
2017-06-11 11:46:51 +0800

    添加初始化主频为72MHZ

--- xiaoxiaoyang/main.cpp
+++ xiaoxiaoyang/main.cpp
@@ -26,6 +26,89 @@ extern I2C_HandleTypeDef hi2c1;
 extern "C"
 #endif
 
+
+#if defined(STM32F102x6) || defined(STM32F102xB) || defined(STM32F103x6) || defined(STM32F103xB) || defined(STM32F103xG) || defined(STM32F103xE)
+
+void SystemClock_Config(void)
+{
+	RCC_ClkInitTypeDef clkinitstruct = { 0 };
+	RCC_OscInitTypeDef oscinitstruct = { 0 };
+	RCC_PeriphCLKInitTypeDef rccperiphclkinit = { 0 };
+
+	/* Enable HSE Oscillator and activate PLL with HSE as source */
+	oscinitstruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	oscinitstruct.HSEState = RCC_HSE_ON;
+	oscinitstruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+	oscinitstruct.PLL.PLLMUL = RCC_PLL_MUL9;
+
+	oscinitstruct.PLL.PLLState = RCC_PLL_ON;
+	oscinitstruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+
+	if (HAL_RCC_OscConfig(&oscinitstruct) != HAL_OK)
+	{
+		asm("bkpt 255");
+	}
+
+	/* USB clock selection */
+	rccperiphclkinit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+	rccperiphclkinit.UsbClockSelection = RCC_USBPLLCLK_DIV1_5;
+	HAL_RCCEx_PeriphCLKConfig(&rccperiphclkinit);
+
+	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+	clocks dividers */
+	clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+	clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	clkinitstruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_2) != HAL_OK)
+	{
+		asm("bkpt 255");
+	}
+}
+
+#else
+
+void SystemClock_Config(void)
+{
+	RCC_ClkInitTypeDef clkinitstruct = { 0 };
+	RCC_OscInitTypeDef oscinitstruct = { 0 };
+	RCC_PeriphCLKInitTypeDef rccperiphclkinit = { 0 };
+
+	/* Configure PLLs ------------------------------------------------------*/
+	/* PLL2 configuration: PLL2CLK = (HSE / HSEPrediv2Value) * PLL2MUL = (25 / 5) * 8 = 40 MHz */
+	/* PREDIV1 configuration: PREDIV1CLK = PLL2CLK / HSEPredivValue = 40 / 5 = 8 MHz */
+	/* PLL configuration: PLLCLK = PREDIV1CLK * PLLMUL = 8 * 9 = 72 MHz */
+	/* Enable HSE Oscillator and activate PLL with HSE as source */
+	oscinitstruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	oscinitstruct.HSEState = RCC_HSE_ON;
+	oscinitstruct.HSEPredivValue = RCC_HSE_PREDIV_DIV5;
+	oscinitstruct.PLL.PLLMUL = RCC_PLL_MUL9;
+	oscinitstruct.Prediv1Source = RCC_PREDIV1_SOURCE_PLL2;
+
+	oscinitstruct.PLL.PLLState = RCC_PLL_ON;
+	oscinitstruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	oscinitstruct.PLL2.PLL2State = RCC_PLL2_ON;
+	oscinitstruct.PLL2.HSEPrediv2Value = RCC_HSE_PREDIV2_DIV5;
+	oscinitstruct.PLL2.PLL2MUL = RCC_PLL2_MUL8;
+	HAL_RCC_OscConfig(&oscinitstruct);
+
+	/* USB clock selection */
+	rccperiphclkinit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+	rccperiphclkinit.UsbClockSelection = RCC_USBPLLCLK_DIV3;
+	HAL_RCCEx_PeriphCLKConfig(&rccperiphclkinit);
+
+	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+	clocks dividers */
+	clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+	clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	clkinitstruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_2);
+}
+#endif
+
 //void SysTick_Handler(void)
 //{
 //	HAL_IncTick();
@@ -44,6 +127,7 @@ void delay()
 
 int main(void)
 {
+		SystemClock_Config();
 		SystemCoreClockUpdate();
 		HAL_Init();
 	
